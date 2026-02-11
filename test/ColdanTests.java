16e5fe5edc3b7435d85b6176d4127a9cb9673139
CSB715 <126495585+CSB715@users.noreply.github.com>
2026-02-10 19:41:54 -0500

    refactoring

---
+++ test/ColdanTests.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+public class ColdanTests {
+
+    @Test
+    public void testHumanGuessesGame() {
+        HumanGuessesGame game = new HumanGuessesGame();
+
+
+    }
+}
